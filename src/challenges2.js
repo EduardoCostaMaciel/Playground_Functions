620630c57a96774e1a4c72440c73af9ded4e506a
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-12-16 19:22:29 -0300

    Feat: Implementa funcao 'techList'

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,14 +1,12 @@
 // Desafio 10
 function techList(tech, name) {
-  let listaDeTech = {};
-  for (let index = 0; index < tech.length; index += 1) {
-    listaDeTech.tech = tech[index];
-    listaDeTech.name = name;
-    
-  }
-  return listaDeTech;
+  let arrayOrdering = tech.sort();
+  let listDeTech = [];
+  arrayOrdering.forEach((element) => {
+    listDeTech.push({ tech: element, name });
+  });
+  return listDeTech;
 }
-console.log(techList(sell,edu));
 
 // Desafio 11
 function generatePhoneNumber() {
