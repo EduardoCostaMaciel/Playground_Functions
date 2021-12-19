2dc43645f226e59659c29ea3582df014631b0df2
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-12-18 21:54:04 -0300

    Feat: Implementa funcao 'generatePhoneNumber'

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,23 +9,40 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function validPhone(arrayNumbers) {
+  if (arrayNumbers.length !== 11) return 'Array com tamanho incorreto.';
+  for (const number of arrayNumbers) {
+    if (arrayNumbers.filter((elem) => elem === number).length >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(arrayNumbers) {
+  if (validPhone(arrayNumbers)) return validPhone(arrayNumbers);
+  arrayNumbers.forEach((elem) => {
+    if (elem < 0 || elem > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  });
+  let strPhone = arrayNumbers.join('');
+  return `(${strPhone.slice(0, 2)}) ${strPhone.slice(2, 7)}-${strPhone.slice(7, 11)}`;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// function triangleCheck() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
 module.exports = {
   generatePhoneNumber,
   techList,
-  hydrate,
-  triangleCheck,
+  // hydrate,
+  // triangleCheck,
 };
